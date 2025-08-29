00c7dfa94a4edf98274a264a64909bf03454d17b
jadeteja <jade.teja@gmail.com>
2025-08-29 18:02:10 +0530

    assignment1 is added

---
+++ Assignments/src/assignment1/Question1.java
@@ -0,0 +1,11 @@
+package assignment1;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello my name is Jade Teja");
+		System.out.println("Iam learning java from scratch");
+	}
+
+}
